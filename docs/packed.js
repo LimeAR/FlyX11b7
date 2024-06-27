682f189f30ac7ad77bd7ffc4133cd020732f90a5
Stephan-Marie Rubayiza <rubayizastefan@gmail.com>
2024-06-27 06:56:05 +0000

    Lens change

--- docs/packed.js
+++ docs/packed.js
@@ -35108,72 +35108,37 @@ function createImageSource(image, options = {}) {
 ;// CONCATENATED MODULE: ./src/main.js
 
 
-const liveRenderTarget = document.getElementById("canvas")
-const flipCamera = document.getElementById("flip")
+(async function () {
+  const cameraKit = await bootstrapCameraKit({ 
+    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzE4OTg3ODg3LCJzdWIiOiJkN2UwNWJjYS02ODVkLTQ0ZGItODE0Mi01ZDg0YTA3YjRlYWJ-UFJPRFVDVElPTn41NTEzNzczNi02YzA2LTRkMjktYjg0YS0wN2Y1MTlhZGRjZGUifQ.sECS_i5kKnojrBmdFNF0r5P8HMnebLJx8P6Xex9VcSE' });
 
-let isBackFacing = true
-let mediaStream
+const session = await cameraKit.createSession();
 
-async function init() {
-  cameraKit = await bootstrapCameraKit({
-    apiToken: "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzE4OTg3ODg3LCJzdWIiOiJkN2UwNWJjYS02ODVkLTQ0ZGItODE0Mi01ZDg0YTA3YjRlYWJ-UFJPRFVDVElPTn41NTEzNzczNi02YzA2LTRkMjktYjg0YS0wN2Y1MTlhZGRjZGUifQ.sECS_i5kKnojrBmdFNF0r5P8HMnebLJx8P6Xex9VcSE"
-  })
+document.getElementById('canvas').replaceWith(session.output.capture);
 
-  session = await cameraKit.createSession({ liveRenderTarget })
- lens = await cameraKit.lensRepository.loadLens(
-    '1f3d84d1-9bca-428b-9818-b3aa3d505904',
+const lens = await cameraKit.lensRepository.loadLens(
+    '5acc3c5f-c8fc-4658-a782-dc191df1c2b4',
     '1ab5269b-f2b0-4570-a30f-74a123521727'
   );
 
-  session.applyLens(lens)
+const mediaStream = await navigator.mediaDevices.getUserMedia({
+  video:{
+    width: { min: 1024, ideal: 1280, max: 1920 },
+    height: { min: 576, ideal: 720, max: 1080 },
+    facingMode: { exact: "environment" },
+  },
+});
 
-  bindFlipCamera(session)
-}
+const source = createMediaStreamSource(mediaStream, { cameraType: 'environment' 
+})
 
-function bindFlipCamera(session) {
-  flipCamera.style.cursor = "pointer"
+await session.setSource(source);
 
-  flipCamera.addEventListener("click", () => {
-    updateCamera(session)
-  })
+session.play('capture');
 
-  updateCamera(session)
-}
+await session.applyLens(lens);
 
-async function updateCamera(session) {
-  isBackFacing = !isBackFacing
-
-  flipCamera.innerText = isBackFacing
-    ? "Switch to Front Camera"
-    : "Switch to Back Camera"
-
-  if (mediaStream) {
-    session.pause()
-    mediaStream.getVideoTracks()[0].stop()
-  }
-
-  mediaStream = await navigator.mediaDevices.getUserMedia({
-    video: {
-      facingMode: isBackFacing ? "environment" : "user"
-    }
-  })
-
-  source = createMediaStreamSource(mediaStream, {
-    // NOTE: This is important for world facing experiences
-    cameraType: isBackFacing ? "back" : "front"
-  })
-
-  await session.setSource(source)
-
-  if (!isBackFacing) {
-    source.setTransform(Transform2D.MirrorX)
-  }
-
-  session.play()
-}
-
-init()
-
+})();
 })();
 
 /******/ })()
